f927d4f5dc09da9eb1cacd3c2da8d8bfa9986128
thanakarn <thanakarn@artisan.co.th>
2020-09-03 17:41:32 +0700

    Fix Confict_listCar

--- react-app/src/components/ListCar.js
+++ react-app/src/components/ListCar.js
@@ -1,7 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import 'antd/dist/antd.css'
+<<<<<<< Updated upstream
 import { Card, Avatar } from 'antd'
+=======
+import { Card, Avatar } from 'antd';
+
+const { Meta } = Card;
+>>>>>>> Stashed changes
 
 export default function CarsList() {
   const { Meta } = Card
@@ -28,39 +34,40 @@ export default function CarsList() {
     },
   }
   return (
-    // <div>
-    //   <div>
-    //     <h4>Cars List</h4>
-    //     <ul>
-    //       {cars &&
-    //         cars.map((car, index) => (
-    //           <li key={index}>
-    //             <p>
-    //               {index + 1} ชื่อเจ้าของรถ {car.data.name}{' '}
-    //             </p>
-    //             เลขทะเบียนรถ {car.data.licensePlate}
-    //           </li>
-    //         ))}
-    //     </ul>
-    //   </div>
-    // </div>
-    <div style={style}>
-      <h1>Cars List</h1>
-      <Card
-        style={{ width: 300 }}
-        cover={
-          <img
-            alt="example"
-            src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+
+    <div>
+      {/* <div>
+        <h4>Cars List</h4>
+        <ul>
+          {cars &&
+            cars.map((car, index) => (
+              <li key={index}>
+                <p>
+                  {index + 1} ชื่อเจ้าของรถ {car.data.name}{' '}
+                เลขทะเบียนรถ {car.data.licensePlate}
+                </p>
+              </li>
+            ))}
+        </ul>
+      </div> */}
+      {cars &&
+      cars.map((car,index) =>(
+        <Card
+          style={{ width: 300 }}
+          cover={
+            <img
+              alt="example"
+              src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+            />
+          }
+        >
+          <Meta
+            avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+            title="Card title"
+            description="This is the description"
           />
-        }
-      >
-        <Meta
-          avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-          title="เจ้าของรถ"
-          description="This is the description"
-        />
-      </Card>
+        </Card>
+      ))}
     </div>
   )
 }
